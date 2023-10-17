1c8fef7745dee9c477670bbe0d4402ebc79af78a
SitdePykee <darkgamers751@gmail.com>
2023-10-17 20:30:37 +0700

    Up

---
+++ main.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, session
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = "27eduCBA09"
+
+
+#================================================================================================================================
+def check_exists(username,password):
+    result = False
+    sqldbname = 'db/website.db'
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+
+    sqlcommand = "Select * from user where name = '"+username+"' and password = '"+password+"'"
+    cursor.execute(sqlcommand)
+    data = cursor.fetchall()
+    if len(data) > 0:
+        result = True
+    conn.close()
+    return result
+
+def check_register(username, password):
+    error = False
+    sqldbname = 'db/website.db'
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+
+    sqlcommand = "Select * from user where name = '" + username + "'"
+    cursor.execute(sqlcommand)
+    data = cursor.fetchall()
+    if len(data) > 0:
+        error = True
+    conn.close()
+    return error
+def insert_user(username, email, password):
+    sqldbname = 'db/website.db'
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+    sqlcommand = "Insert into user(name,email,password) values ('" + username + "', '" + username + "@gmail.com', '" + password + "')"
+    cursor.execute(sqlcommand)
+    conn.commit()
+    conn.close()
+
+#================================================================================================================================
+
+@app.route("/")
+def Index():
+    username = ""
+    return render_template("Index.html", username=username)
+
+#================================================================================================================================
+
+@app.route("/login")
+def formlogin():
+    return render_template("login.html")
+
+@app.route("/login",methods=['POST'])
+def login():
+    error = False
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if check_exists(username, password):
+            session['username'] = username
+            return render_template("Index.html", username=session['username'])
+        else:
+            error = True
+    return render_template("login.html", error=error)
+
+#================================================================================================================================
+
+@app.route("/register")
+def registerform():
+    return render_template("register.html")
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    error = check_register(username, password)
+    if not error:
+        insert_user(username, email, password)
+        return render_template("Index.html")
+    else:
+        return render_template("register.html", error = error)
+
+#================================================================================================================================
+
+
+if __name__ == '__main__':
+    app.run()
