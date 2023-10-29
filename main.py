09339b4a073c55e4ac7fae4ea88d211586a1c9b2
SitdePykee <darkgamers751@gmail.com>
2023-10-29 16:37:22 +0700

    m

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -70,7 +70,6 @@ def get_items():
     items_per_page = 12
     page = request.args.get('page', 1, type=int)
     start_index = (page - 1) * items_per_page
-    end_index = start_index + items_per_page
 
     sqldbname = 'db/clothes.db'
     conn = sqlite3.connect(sqldbname)
@@ -84,17 +83,21 @@ def get_items():
 
     total_pages = len(data) // items_per_page + (1 if len(data) % items_per_page > 0 else 0)
     current_page_items = data[start_index:start_index + min(items_per_page, len(data) - start_index)]
-
-    return current_page_items, total_pages, page
+    number_of_item = len(current_page_items)
+    return current_page_items, total_pages, page, number_of_item
 
 #================================================================================================================================
 
 @app.route("/")
 def Index():
-    username = ""
-    current_page_items, total_pages, page = get_items()
-    return render_template("Index.html",clothes = current_page_items,
-                           total_pages = total_pages, current_page = page)
+    current_page_items, total_pages, page, number_of_item = get_items()
+    if 'username' in session:
+        return render_template("logged-in-index.html", username=session['username'],
+                                   clothes = current_page_items,total_pages = total_pages, current_page = page,
+                                   number_of_item = number_of_item)
+    else:
+        return render_template("Index.html",clothes = current_page_items,
+                           total_pages = total_pages, current_page = page, number_of_item = number_of_item)
 
 #================================================================================================================================
 
@@ -110,13 +113,19 @@ def login():
         password = request.form['password']
         if check_exists(username, password):
             session['username'] = username
-            current_page_items, total_pages, page = get_items()
+            current_page_items, total_pages, page, number_of_item = get_items()
             return render_template("logged-in-index.html", username=session['username'],
-                                   clothes = current_page_items,total_pages = total_pages, current_page = page)
+                                   clothes = current_page_items,total_pages = total_pages, current_page = page,
+                                   number_of_item = number_of_item)
         else:
             error = True
     return render_template("login.html", error=error)
 
+#================================================================================================================================
+@app.route("/logout")
+def log_out():
+    session.pop('username', None)
+    return redirect(url_for('Index'))
 #================================================================================================================================
 
 @app.route("/register")
@@ -130,16 +139,15 @@ def register():
     error = check_register(username, password)
     if not error:
         insert_user(username, email, password)
-        current_page_items, total_pages, page = get_items()
+        current_page_items, total_pages, page, number_of_item = get_items()
         return render_template("Index.html",clothes = current_page_items,total_pages = total_pages,
-                               current_page = page)
+                               current_page = page, number_of_item = number_of_item)
     else:
         return render_template("register.html", error = error)
-
+#================================================================================================================================
 @app.route("/search")
 def search_clothes_form():
     return render_template("search.html")
-
 @app.route("/search", methods=["POST"])
 def search_result():
     search_text = request.form['search_text']
